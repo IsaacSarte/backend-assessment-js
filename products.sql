47d0760c694a562d611092c339024790ff76f009
Isaac Sarte <82153590+IsaacSarte@users.noreply.github.com>
2024-11-01 15:37:56 +0800

    add exported sql file

---
+++ products.sql
@@ -0,0 +1,28 @@
+CREATE TABLE mytable(
+   id         INTEGER  NOT NULL PRIMARY KEY 
+  ,product_id INTEGER  NOT NULL
+  ,title      VARCHAR(65) NOT NULL
+  ,tags       VARCHAR(39)
+  ,created_at DATE  NOT NULL
+  ,updated_at DATE  NOT NULL
+  ,sku        VARCHAR(15) NOT NULL
+);
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (59,9505912586529,'The Compare at Price Snowboard Medium med-2','Accessory, Sport, Winter','2024-07-28','2024-07-28','med-2');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (61,9505912324385,'The Complete Snowboard Powder comp-3','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-3');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (62,9505912324385,'The Complete Snowboard Sunset comp-5','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-5');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (63,9505912357153,'The Hidden Snowboard Default Title hide-2','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','hide-2');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (64,9505912422689,'The Draft Snowboard Default Title draft-1',NULL,'2024-06-28','2024-06-28','draft-1');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (65,9505912324385,'The Complete Snowboard Dawn comp-2','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-2');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (66,9505912324385,'The Complete Snowboard Electric comp-4','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-4');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (67,9505912586529,'The Compare at Price Snowboard Large lar-2','Accessory, Sport, Winter','2024-07-28','2024-07-28','lar-2');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (68,9505912586529,'The Compare at Price Snowboard Medium med-2','Accessory, Sport, Winter','2024-07-28','2024-07-28','med-2');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (69,9505912553761,'The Inventory Not Tracked Snowboard Default Title sku-untracked-1','Accessory, Sport, Winter','2024-06-28','2024-06-28','sku-untracked-1');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (70,9505912324385,'The Complete Snowboard Electric comp-4','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-4');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (71,9505912324385,'The Complete Snowboard Powder comp-3','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-3');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (72,9505912357153,'The Hidden Snowboard Default Title hide-2','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','hide-2');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (73,9505912455457,'The Out of Stock Snowboard Default Title oos-1','Accessory, Sport, Winter','2024-06-28','2024-06-28','oos-1');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (74,9505912324385,'The Complete Snowboard Dawn comp-2','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-2');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (75,9505912422689,'The Draft Snowboard Default Title draft-1',NULL,'2024-06-28','2024-06-28','draft-1');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (76,9505912324385,'The Complete Snowboard Ice comp-1','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-1');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (77,9505912586529,'The Compare at Price Snowboard Large lar-2','Accessory, Sport, Winter','2024-07-28','2024-07-28','lar-2');
+INSERT INTO mytable(id,product_id,title,tags,created_at,updated_at,sku) VALUES (78,9505912324385,'The Complete Snowboard Sunset comp-5','Premium, Snow, Snowboard, Sport, Winter','2024-06-28','2024-06-28','comp-5');
